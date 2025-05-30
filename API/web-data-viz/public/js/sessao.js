b37bf899f4c3e0ec16452b8f72f1d3ee79a358b5
JuanSanchezVll <juan.sanchez@sptech.school>
2025-05-30 14:50:39 -0300

    Adicionando login/cadastro

--- API/web-data-viz/public/js/sessao.js
+++ API/web-data-viz/public/js/sessao.js
@@ -2,13 +2,11 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-    var indexNome = sessionStorage.NOME_USUARIO.indexOf(" ")
-    
 
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = sessionStorage.NOME_USUARIO.slice(0, indexNome)
+        b_usuario.innerHTML = nome;
     } else {
         window.location = "../login.html";
     }
