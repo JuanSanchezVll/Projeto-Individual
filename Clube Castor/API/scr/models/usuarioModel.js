da01548523423d198b2bb78b2b0622fe9ec6cf74
JuanSanchezVll <juan.sanchez@sptech.school>
2025-06-02 05:27:06 -0300

    Adicionando modificações MYSQL API

--- Clube Castor/API/scr/models/usuarioModel.js
+++ Clube Castor/API/scr/models/usuarioModel.js
@@ -3,11 +3,19 @@ var database = require("../database/config");
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha) 
     
-    var instrucaoSqlMembro = `
-        SELECT idMembro, nome, date(dtNasc) as dtNasc, telefone, email, senha FROM membro WHERE email = '${email}' AND senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSqlMembro);
-    return database.executar(instrucaoSqlMembro);
+    // var instrucaoSqlMembro = `
+    //     SELECT ID as IdUsuario, nome, date(dtNasc) as dtNasc, telefone, email, senha 
+    //     FROM membro 
+    //     WHERE email = '${email}' AND senha = '${senha}';
+    // `;
+
+    var query = `SELECT ID as IdUsuario, Nome, Email, Telefone, Dt_Nascimento as DataNascimento, Senha, ID_CARGO as IdCargo, 
+                        ID_UNIDADE as IdUnidade 
+                FROM USUARIO
+                WHERE Email = ${email} AND Senha = ${senha}`;
+
+    console.log("Executando a instrução SQL: \n" + query);
+    return database.executar(query);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
@@ -19,54 +27,43 @@ async function cadastrar(nome, dtNasc, telefone, email, senha, cep, rua, numero,
     try {
 
         var instrucaoSqlMembro = `
-        INSERT INTO membro (nome, dtNasc, telefone, email, senha) VALUES ('${nome}', '${dtNasc}', '${telefone}', '${email}', '${senha}');
+        INSERT INTO USUARIO (nome, email, telefone, Dt_nascimento, senha, id_cargo, id_unidade) VALUES ('${nome}', '${email}', '${telefone}', '${dtNasc}', '${senha}', ${idCargo}, ${idUnidade});
         `;
 
         console.log("Executando a instrução SQL: \n" + instrucaoSqlMembro);
-        await database.executar(instrucaoSqlMembro)
+        await database.executar(instrucaoSqlMembro);
 
-        var instrucaoSqlEndereco = `INSERT INTO endereco (fkMembro, cep, rua, numero, complemento, bairro, cidade, estado) VALUES 
-        ((select idMembro from membro order by idMembro desc limit 1), '${cep}', '${rua}', '${numero}', '${complemento}', '${bairro}', '${cidade}', '${estado}');`
+        var queryEndereco = `INSERT INTO ENDERECO (CEP, LOGRADOURO, NUMERO, BAIRRO, CIDADE, ESTADO, COMPLEMENTO, ID_USUARIO) VALUES ('${cep}','${rua}','${numero}','${bairro}','${cidade}','${estado}','${complemento}',(select ID from USUARIO order by ID desc limit 1));`
 
-        console.log("Executando a instrução SQL: \n" + instrucaoSqlEndereco);
-        await database.executar(instrucaoSqlEndereco);
+        console.log("Executando a instrução SQL: \n" + queryEndereco);
+        await database.executar(queryEndereco);
 
+        var queryInsertClasse = `INSERT INTO USUARIO_CLASSE (ID_CLASSE, ID_USUARIO) VALUES`
 
-        var instrucaoSqlCargoMembro = `
-            INSERT INTO cargoMembro (fkMembro, fkCargo) VALUES
-            ((select idMembro from membro order by idMembro desc limit 1), (select idCargo from cargo where nome = '${cargo}'));`
-
-        console.log("Executando a instrução SQL: \n" + instrucaoSqlCargoMembro);
-        await database.executar(instrucaoSqlCargoMembro);
-
-        var instrucaoSqlClasseMembro = `INSERT INTO classeMembro (fkMembro, fkClasse) VALUES`
-
-        for (var classeAtual = 0; classeAtual < listaClasses.length; classeAtual += 1) {
-
-            if (classeAtual != listaClasses.length - 1) {
-                if (listaClasses[classeAtual] < 10) {
-                    instrucaoSqlClasseMembro += `
-            ((select idMembro from membro order by idMembro desc limit 1), 1000${listaClasses[classeAtual]}),
-            `
-                } else {
-                    instrucaoSqlClasseMembro += `
-            ((select idMembro from membro order by idMembro desc limit 1), 100${listaClasses[classeAtual]}),
-            `
-                }
-            } else if (listaClasses[classeAtual] < 10) {
-                instrucaoSqlClasseMembro += `
-            ((select idMembro from membro order by idMembro desc limit 1), 1000${listaClasses[classeAtual]});
-            `
-            } else {
-                instrucaoSqlClasseMembro += `
-            ((select idMembro from membro order by idMembro desc limit 1), 100${listaClasses[classeAtual]});
-            `
-            }
+        for(classe = 0; classe < listaClasses.length; classe++ )
+        {
+          if(!(classe == listaClasses.length -1)){
+            queryInsertClasse += `((select idMembro from membro order by idMembro desc limit 1), ${listaClasses[classe]}),`;
+          }
+          queryInsertClasse += `((select idMembro from membro order by idMembro desc limit 1), ${listaClasses[classe]});`;
         }
 
-        console.log("Executando a instrução SQL: \n" + instrucaoSqlClasseMembro);
-        await database.executar(instrucaoSqlClasseMembro);
+        console.log("Executando a instrução SQL: \n" + queryInsertClasse);
+        await database.executar(queryInsertClasse);
 
+        
+        var queryInsertEspecialidade = `INSERT INTO USUARIO_ESPECIALIDADE (ID_ESPECIALIDADE, ID_USUARIO) VALUES`
+
+        for(classe = 0; classe < listaClasses.length; classe++ )
+        {
+          if(!(classe == listaClasses.length -1)){
+            queryInsertClasse += `((select idMembro from membro order by idMembro desc limit 1), ${listaClasses[classe]}, NOW()),`;
+          }
+          queryInsertClasse += `((select idMembro from membro order by idMembro desc limit 1), ${listaClasses[classe]}, NOW());`;
+        }
+
+        console.log("Executando a instrução SQL: \n" + queryInsertEspecialidade);
+        await database.executar(queryInsertEspecialidade);
 
         var instrucaoSqlEspecialidadeMembro = `INSERT INTO especialidadeMembro (fkMembro,fkEspecialidade) VALUES`
 
